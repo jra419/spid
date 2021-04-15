d1c0fca2a72ac54010d62e50513f684a71576661
Shinkirou <joao.f.amado@gmail.com>
2021-04-15 17:54:59 +0100

    Global counters for evaluation metrics, updated per flow. Only start running the pipeline with > 10 flows. Various checks for active ML algorithms.

--- spid_ml/config.py
+++ spid_ml/config.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import pandas as pd
 import argparse
+from datetime import datetime
 
-# Dataframe with the values received from the data plane, aggregated by IP
+# Dataframe with the values received from the data plane, aggregated by IP.
 df = pd.DataFrame(columns=['ip_src', 'ip_dst', 'cm_ip', 'cm_ip_port_21', 'cm_ip_port_22', 'cm_ip_port_80',
                            'cm_ip_tcp_syn', 'cm_ip_tcp_ack', 'cm_ip_tcp_rst', 'cm_ip_icmp', 'bm_ip_src', 'bm_ip_dst',
                            'bm_ip_src_port_src', 'bm_ip_src_port_dst', 'bm_ip_dst_port_src', 'bm_ip_dst_port_dst'])
 
-# Final dataframe containing both the original values and the cluster results & postprocessing
+# Final dataframe containing both the original values and the cluster results & postprocessing.
 df_final_combined = pd.DataFrame(columns=['ip_src', 'ip_dst', 'cm_ip', 'cm_ip_port_21', 'cm_ip_port_22',
                                           'cm_ip_port_80', 'cm_ip_tcp_syn', 'cm_ip_tcp_ack', 'cm_ip_tcp_rst',
                                           'cm_ip_icmp', 'bm_ip_src', 'bm_ip_dst', 'bm_ip_src_port_src',
@@ -22,13 +23,30 @@ norm = pd.DataFrame()
 spid_stats = pd.DataFrame()
 spid_stats_norm = pd.DataFrame()
 
-# Dataframes containing the output of the ML algorithms
+# Dataframes containing the output of the ML algorithms.
 df_kmeans_final = pd.DataFrame()
 df_dbscan_final = pd.DataFrame()
 
-# Temporary dataframes holding the flows corresponding to isolated clusters
+# Temporary dataframes holding the flows corresponding to isolated clusters.
 df_kmeans_isolated = pd.DataFrame()
 df_dbscan_isolated = pd.DataFrame()
+
+# Evaluation metrics: global counters.
+kmeans_tp = 0
+kmeans_fp = 0
+kmeans_tn = 0
+kmeans_fn = 0
+dbscan_tp = 0
+dbscan_fp = 0
+dbscan_tn = 0
+dbscan_fn = 0
+all_tp = 0
+all_fp = 0
+all_tn = 0
+all_fn = 0
+
+# Datetime variable, used to output files with the same date and time.
+now = datetime.now()
 
 parser = argparse.ArgumentParser(description='SPID ML pipeline script args.')
 
